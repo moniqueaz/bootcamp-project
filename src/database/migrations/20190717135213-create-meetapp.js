9ec185c1112b53b81dfde929deaa8ac31f18457d
Monique Azevedo <moniqueaz.si@gmail.com>
2019-07-17 11:07:54 -0300

    Cria migration de criar meetapp

---
+++ src/database/migrations/20190717135213-create-meetapp.js
@@ -0,0 +1,62 @@
+/**
+ * O usuário pode cadastrar meetups na plataforma com
+ *
+ * título do meetup,
+ * descrição,
+ * localização,
+ * data e hora
+ * imagem (banner).
+ *
+ * Todos campos são
+ * obrigatórios. Adicione também um campo
+ *
+ * user_id
+ *
+ * que armazena o ID do usuário
+ * que organiza o evento.
+ */
+
+module.exports = {
+  up: (queryInterface, Sequelize) => {
+    return queryInterface.createTable('meetapp', {
+      id: {
+        type: Sequelize.INTEGER,
+        allowNull: false,
+        autoIncrement: true,
+        primaryKey: true,
+      },
+      title: {
+        type: Sequelize.STRING,
+        allowNull: false,
+      },
+      description: {
+        type: Sequelize.TEXT,
+        allowNull: false,
+      },
+      date: {
+        type: Sequelize.DATE,
+        allowNull: false,
+      },
+      path: {
+        type: Sequelize.STRING,
+        allowNull: false,
+      },
+      user_id: {
+        type: Sequelize.INTEGER,
+        allowNull: false,
+      },
+      created_at: {
+        type: Sequelize.DATE,
+        allowNull: false,
+      },
+      updated_at: {
+        type: Sequelize.DATE,
+        allowNull: false,
+      },
+    });
+  },
+
+  down: queryInterface => {
+    return queryInterface.dropTable('meetapp');
+  },
+};
